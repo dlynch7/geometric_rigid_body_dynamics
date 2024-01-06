84ea5d8f96ad5ed1a38c64d5ae15b9eed0abf444
dlynch7 <daniellynch2021@u.northwestern.edu>
2024-01-06 17:11:02 -0600

    modified initial conditions, etc., to generate figures for demonstration

--- src/MATLAB/main.m
+++ src/MATLAB/main.m
@@ -29,12 +29,12 @@ params = init_params(body_name,dimensions);
 Rwb_init = eye(3);
 Twb_init = [Rwb_init,   zeros(3,1);
             zeros(1,3), 1]; % CoM frame coincident with world frame
-Vb_init = [10;-10;10;0;0;0];
+Vb_init = [1;2;3;1;2;3];
 Fb = zeros(6,1);
         
 dt = 1e-3; % timestep size
 t0 = 0;
-tf = 1; % simulate for 1 second
+tf = 10; % simulate for 1 second
 t = t0:dt:tf;
 
 %% Simulate via numerical integration
@@ -51,13 +51,20 @@ Vb_hist = NaN(6,numel(t));
 p_hist = NaN(3,numel(t));
 v_hist = NaN(3,numel(t));
 
+% wrench in {b} and {c} frames:
+Fc_hist = NaN(6,numel(t));
+Fb_hist = NaN(6,numel(t));
+
 % determinant of Rwb:
 detRwb_hist = NaN(1,numel(t));
+distToSE3_hist = NaN(1,numel(t));
 
 % energy and momenta:
 E_hist = NaN(1,numel(t)); % mechanical energy
-L_hist = NaN(3,numel(t)); % angular momentum in body frame
-P_hist = NaN(3,numel(t)); % linear momentum in body frame
+Lb_hist = NaN(3,numel(t)); % angular momentum in body frame
+Pb_hist = NaN(3,numel(t)); % linear momentum in body frame
+Lw_hist = NaN(3,numel(t)); % angular momentum in world frame
+Pw_hist = NaN(3,numel(t)); % linear momentum in world frame
 
 for k = 1:numel(t)
     Rwb = Twb{k}(1:3,1:3); % orientation of {b} in {w}
@@ -71,9 +78,12 @@ for k = 1:numel(t)
     
     % momentum:
     Pb = Gb*Vb{k};
-    Pw = transpose(Adjoint(TransInv(Twb{k})))*Gb*Vb{k};
-    L_hist(:,k) = [eye(3),zeros(3)]*Pw; % angular momentum
-    P_hist(:,k) = [zeros(3),eye(3)]*Pw; % linear momentum
+    Lb_hist(:,k) = [eye(3),zeros(3)]*Pb; % angular momentum in {b}
+    Pb_hist(:,k) = [zeros(3),eye(3)]*Pb; % linear momentum in {b}
+    Tbw = TransInv(Twb{k});
+    Pw = transpose(Adjoint(Tbw))*Pb;
+    Lw_hist(:,k) = [eye(3),zeros(3)]*Pw; % angular momentum in {w}
+    Pw_hist(:,k) = [zeros(3),eye(3)]*Pw; % linear momentum in {w}
     
     % velocity of CoM in world frame:
     v = Rwb*Vb{k}(4:6);
@@ -83,6 +93,7 @@ for k = 1:numel(t)
     p_hist(:,k) = pwb;
     v_hist(:,k) = v;
     detRwb_hist(k) = det(Rwb);
+    distToSE3_hist(k) = DistanceToSE3(Twb{k});
     E_hist(k) = E;
 end
 
@@ -90,34 +101,58 @@ end
 % handy diagnostic functions:
 %   total energy
 %   angular momenta
-%   linear momenta in x and y (but not z)
+%   linear momenta in x and y (but not z, b/c of gravity)
 %   determinant of Rwb (how close is it to 1?)
+%   distance to SE(3) manifold (based on Frobenius norm of Twb)
 
 figure;
-plot(t,detRwb_hist,'k-','LineWidth',2)
-ylabel('$\mathrm{det}\left(R_\mathrm{wb}\right)$')
+plot(t,detRwb_hist,'k:',t,distToSE3_hist,'k-','LineWidth',2)
+legend('$\mathrm{det}\left(R_\mathrm{wb}\right)$',...
+    'distance to $SE(3)$','Location','Best')
 xlabel('time $t$ [s]')
+axis([t(1) t(end) -0.5 1.5])
 
 figure;
-subplot(3,1,1)
 plot(t,E_hist,'k-','LineWidth',2)
 ylabel('Energy [J]')
+xlabel('time $t$ [s]')
+axis([t(1) t(end) 0 1.5*max(E_hist)])
 
-subplot(3,1,2)
-plot(t,L_hist(1,:),'r-',...
-     t,L_hist(2,:),'g-',...
-     t,L_hist(3,:),'b-',...
+figure;
+subplot(2,2,1)
+plot(t,Lb_hist(1,:),'r-',...
+     t,Lb_hist(2,:),'g-',...
+     t,Lb_hist(3,:),'b-',...
     'LineWidth',2)
 legend('$L_x$','$L_y$','$L_z$','Location','Best')
-ylabel('Angular momentum [kg m$^2$/s]')
+ylabel('Angular momentum $L_\mathrm{b}$ [kg m$^2$/s]')
+title('Momentum in body frame')
 
-subplot(3,1,3)
-plot(t,P_hist(1,:),'r-',...
-     t,P_hist(2,:),'g-',...
-     t,P_hist(3,:),'b-',...
+subplot(2,2,3)
+plot(t,Pb_hist(1,:),'r-',...
+     t,Pb_hist(2,:),'g-',...
+     t,Pb_hist(3,:),'b-',...
     'LineWidth',2)
 legend('$P_x$','$P_y$','$P_z$','Location','Best')
-ylabel('Linear momentum [kg m/s]')
+ylabel('Linear momentum $P_\mathrm{b}$ [kg m/s]')
+xlabel('time $t$ [s]')
+
+subplot(2,2,2)
+plot(t,Lw_hist(1,:),'r-',...
+     t,Lw_hist(2,:),'g-',...
+     t,Lw_hist(3,:),'b-',...
+    'LineWidth',2)
+legend('$L_x$','$L_y$','$L_z$','Location','Best')
+ylabel('Angular momentum $L_\mathrm{w}$ [kg m$^2$/s]')
+title('Momentum in world frame')
+
+subplot(2,2,4)
+plot(t,Pw_hist(1,:),'r-',...
+     t,Pw_hist(2,:),'g-',...
+     t,Pw_hist(3,:),'b-',...
+    'LineWidth',2)
+legend('$P_x$','$P_y$','$P_z$','Location','Best')
+ylabel('Linear momentum $P_\mathrm{w}$ [kg m/s]')
 xlabel('time $t$ [s]')
 
 figure;
@@ -144,7 +179,7 @@ plot(t,Vb_hist(1,:),'r-',...
      t,Vb_hist(3,:),'b-',...
     'LineWidth',2);
 legend('$\omega_x$','$\omega_y$','$\omega_z$','Location','Best')
-ylabel('angular velocity $\omega$ [rad/s] in $\left\{b\right\}$');
+ylabel('angular velocity [rad/s] in $\left\{\mathrm{b}\right\}$');
 xlabel('time $t$ [s]')
 
 subplot(2,2,2)
@@ -153,7 +188,7 @@ plot(t,p_hist(1,:),'r-',...
      t,p_hist(3,:),'b-',...
      'LineWidth',2);
 legend('$p_x$','$p_y$','$p_z$','Location','Best')
-ylabel('position $p$ [m]');
+ylabel('COM position in $\left\{\mathrm{w}\right\}$ [m]');
 
 subplot(2,2,4)
 plot(t,v_hist(1,:),'r-',...
@@ -161,7 +196,7 @@ plot(t,v_hist(1,:),'r-',...
      t,v_hist(3,:),'b-',...
     'LineWidth',2);
 legend('$v_x$','$v_y$','$v_z$','Location','Best')
-ylabel('velocity $v$ [m/s] in $\left\{w\right\}$');
+ylabel('COM velocity [m/s] in $\left\{\mathrm{w}\right\}$');
 xlabel('time $t$ [s]')
 
 end
